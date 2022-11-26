c99ab04ab3566a7380e6b9ce5d8bc595e675a886
Lukasz Szulik <lukaszgit@o2.pl>
2022-11-26 13:47:34 +0100

    update react-redux

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,38 +4,9 @@ import Column from '../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux';
 
-const columns = useSelector(state => state.columns);
 
 const List = () => {
-	const [columns, setColumns] = ([
-		{
-			id: 1,
-			title: 'Books',
-			icon: 'book',
-			cards: [
-				{ id: 1, title: 'This is Going to Hurt' },
-				{ id: 2, title: 'Interpreter of Maladies' }
-			]
-		},
-		{
-			id: 2,
-			title: 'Movies',
-			icon: 'film',
-			cards: [
-				{ id: 1, title: 'Harry Potter' },
-				{ id: 2, title: 'Star Wars' }
-			]
-		},
-		{
-			id: 3,
-			title: 'Games',
-			icon: 'gamepad',
-			cards: [
-				{ id: 1, title: 'The Witcher' },
-				{ id: 2, title: 'Skyrim' }
-			]
-		}
-	]);
+	const columns = useSelector(state => state.columns);
 
 return (
   <div className={styles.list}>
