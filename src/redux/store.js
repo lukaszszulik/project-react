89dcd2b9e11c07fd89b7fee325c12d53b575675e
Lukasz Szulik <lukaszgit@o2.pl>
2022-11-28 23:34:06 +0100

    add reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,16 @@
 import { createStore } from 'redux';
+import shortid from 'shortid';
 import initialState from './initialState';
 
 const reducer = (state, action) => {
+  switch(action.type){
+    case 'ADD_COLUMN':
+      return { ...state, columns: [...state.columns, {...action.payload, id: shortid()}]}
+      case 'ADD_CARD':
+        return {...state, cards: [...state.cards, {id: shortid(), columnId: action.payload.columnId, title: action.payload.title}]}
+      default:
   return state;
+  }
 };
 
 const store = createStore(
