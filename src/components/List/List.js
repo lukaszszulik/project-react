42230eddb4f2c1886a624cddc87dcdefd649f4e1
Lukasz Szulik <lukaszgit@o2.pl>
2022-12-05 14:29:42 +0100

    add error.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,27 +3,31 @@ import Column from '../Column/Column';
 //import { useEffect } from 'react';
 import ColumnForm from '../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux';
-import { getAllColumns } from '../../redux/store';
+import { getListById, getColumnsByList } from '../../redux/store';
 
 
 const List = () => {
-	const columns = useSelector(getAllColumns);
+	const columns = useSelector(getColumnsByList);
+
+  const listData = useSelector(getListById);
 
 return (
   <div className={styles.list}>
     <header className={styles.header}>
-      <h2 className={styles.title}>Things to do<span>soon!</span></h2>
+      <h2 className={styles.title}>
+        dd
+        
+      </h2>
     </header>
+
     <p className={styles.description}>Interesting things I want to check out</p>
+
     <section className={styles.columns}>
-      {columns.map(column =>
-        <Column
-          key={column.id}
-          {...column}  />
-      )}
+      {columns.map(column => <Column key={column.id} {...column}  /> )}
     </section>
     <ColumnForm />
   </div>
+  
 );
 };
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -9,6 +9,10 @@ export const getFilteredCards = ({ cards, searchString }, columnId) => cards
 
   export const getAllColumns = (state) => state.columns;
 
+  export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId);
+
+  export const getColumnsByList = ({columns}, listId) =>columns.filter((column) => column.listId === listId);
+
   // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 
